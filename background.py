6311cb10bc6856f1b7fa493f3ee652b6f8f0e6b8
codeOlam <frankuchenna30@gmail.com>
2025-06-23 22:00:43 +0100

    cleaned code

--- background.py
+++ background.py
@@ -1,7 +1,6 @@
 import asyncio
 import httpx
-from utils import TELEGRAM_API_URL, format_price, get_price_with_change, price_cache, TOKENS, CHANNEL_ID, \
-    fetch_all_coinlore_prices
+from utils import TELEGRAM_API_URL, format_price, get_price_with_change, TOKENS, CHANNEL_ID, fetch_all_coinlore_prices
 
 
 async def start_price_checker():
@@ -36,9 +35,9 @@ def build_message(prices):
     lines = [
         "*WKC Watcher 👀*\n🚀Live Token Prices🚀\n",
         line("wiki-cat"), "──────────────────",
-        "*🎖 Commty Watchlist*", *
+        "*🎖 Commty Watchlist*\n", *
         (line(k) for k in ["the-kingdom-coin", "defi-tiger", "bnbtiger"]),
-        "──────────────────", "*🏅 Major Coins*", *
+        "──────────────────", "*🏅 Major Coins*\n", *
         (line(k)
          for k in ["bitcoin", "ethereum", "ripple", "binancecoin", "solana"]),
         "──────────────────",
@@ -54,118 +53,3 @@ def build_message(prices):
 async def send_to_channel(msg):
     async with httpx.AsyncClient() as c:
         await c.post(f"{TELEGRAM_API_URL}/sendMessage", json={"chat_id": CHANNEL_ID, "text": msg, "parse_mode": "Markdown"})
-
-# def build_message(prices):
-#     def line(k):
-#         t = TOKENS[k]
-#         price = prices.get(k, "N/A")
-#         chg = price_cache.get(k, {}).get("chg_24", "—")
-#         if chg:
-#             try:
-#                 indicator = "💹" if float(chg.replace('%', '')) >= 0 else "🔻"
-#             except:
-#                 indicator = ""
-#         return f"{t['emoji']} *{t['display_name']}*: `${format_price(price)}` {indicator} ({chg}) 24h"
-
-#     lines = ["*WKC Watcher 👀*\n🚀Live Token Prices🚀\n",
-#              line("wiki-cat"), "──────────",
-#              "*🎖 Commty Watchlist*", *
-#              (line(k) for k in ["the-kingdom-coin", "defi-tiger", "bnbtiger"]),
-#              "──────────", "*🏅 Major Coins*", *
-#              (line(k) for k in [
-#                  "bitcoin",
-#                  "ethereum",
-#                  "ripple",
-#                  "binancecoin",
-#                  "solana"
-#              ]
-#              ),
-#              "──────────",
-#              "🧵 *Follow:*",
-#              "🔗 [Creator on X](https://x.com/codeolam)",
-#              "🔗 [WikiCat on X](https://x.com/wikicatcoin)",
-#              "──────────",
-#              "\n🤖 [More market insights](https://t.me/kodOlam_bot)"]
-#     return "\n".join(lines)
-
-
-# import asyncio
-# from utils import TELEGRAM_API_URL, format_price, price_cache, fetch_all_coinlore_prices, TOKENS, CHANNEL_ID
-# import httpx
-# import os
-
-
-# async def start_price_checker():
-#     # print('[start_price_checker] is being called!')
-#     try:
-#         while True:
-#             await fetch_all_coinlore_prices()
-#             prices = {t: price_cache.get(t, {}).get(
-#                 "price", "—") for t in TOKENS}
-#             await send_to_channel(build_message(prices))
-#             await asyncio.sleep(60)
-#     except asyncio.CancelledError:
-#         # Price checker task was cancelled. Exiting gracefully
-#         raise
-#     except Exception as e:
-#         # print(f"[Background Error] {type(e).__name__}: {e}")
-#         raise  # Let the worker catch and log it too
-
-
-# def build_message(prices):
-#     l = ["*WKC Watcher 👀*\n🚀Live Token Prices🚀\n"]
-#     w = "wiki-cat"
-
-#     def format_line(token_id):
-#         token = TOKENS[token_id]
-#         price = prices.get(token_id, "—")
-#         emoji = token["emoji"]
-
-#         # Get change info
-#         cached = price_cache.get(token_id, {})
-#         chg = cached.get("chg_24")
-#         indicator = ""
-        # if chg:
-        #     try:
-        #         # arrow = "📈" if float(chg.replace('%', '')) >= 0 else "📉"
-        #         indicator = "💹" if float(chg.replace('%', '')) >= 0 else "🔻"
-        #     except:
-        #         indicator = ""
-#         chg_txt = f"({chg})" if chg else ""
-
-#         return f"{emoji} *{token['display_name']}*: `${format_price(price)}` {indicator} {chg_txt} 24h"
-
-#     # Top section: WKC
-#     l.append(format_line(w))
-#     l.append("──────────────────")
-
-#     # Section: New Tokens
-#     l.append("*🎖 Commty Watchlist Tokens*\n")
-#     for k in ["the-kingdom-coin", "defi-tiger", "bnbtiger"]:
-#         l.append(format_line(k))
-#     l.append("──────────────────")
-
-#     # Section: Major Coins
-#     l.append("*🏅 Major Coins/Alts*\n")
-#     for k in ["bitcoin", "ethereum", "ripple", "binancecoin", "solana"]:
-#         l.append(format_line(k))
-
-    # l.append("──────────────────")
-    # l += [
-    #     "🧵 *Follow:*",
-    #     "🔗 [Creator on X](https://x.com/codeolam)",
-    #     "🔗 [WikiCat on X](https://x.com/wikicatcoin)",
-    #     "──────────────────",
-    #     "\n🤖 [More market insights](https://t.me/kodOlam_bot)"
-    # ]
-
-#     return "\n".join(l)
-
-
-# async def send_to_channel(msg):
-#     # print('[send_to_channel] here now')
-#     async with httpx.AsyncClient() as c:
-#         await c.post(
-#             f"{TELEGRAM_API_URL}/sendMessage",
-#             json={"chat_id": CHANNEL_ID, "text": msg, "parse_mode": "Markdown"}
-#         )
