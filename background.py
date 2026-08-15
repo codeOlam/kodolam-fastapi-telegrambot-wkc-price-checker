6b631a03f5a41936c46e2973de2cb0cd4112db04
codeOlam <frankuchenna30@gmail.com>
2026-08-15 16:13:22 +0100

    fix silent killing of task because of bad data

--- background.py
+++ background.py
@@ -1,23 +1,27 @@
 import asyncio
 import os
+import traceback
 import httpx
 from utils import TELEGRAM_API_URL, format_price, get_price_with_change, TOKENS, CHANNEL_ID, fetch_all_coinlore_prices
 
 
 async def start_price_checker():
     while True:
-        await fetch_all_coinlore_prices()
-        prices = {k: await get_price_with_change(k) for k in TOKENS}
-        msg = build_message(prices)
-        buttons = [
-            [{"text": "Creator on X",
-                "url": "https://x.com/codeolam"}],
-            [{"text": "WikiCat on X",
-                "url": "https://x.com/wikicatcoin"}],
-            [{"text": "🤖 Bot Playground",
-                "url": "https://t.me/kodOlam_bot"}],
-        ]
-        await send_to_channel(msg, buttons)
+        try:
+            await fetch_all_coinlore_prices()
+            prices = {k: await get_price_with_change(k) for k in TOKENS}
+            msg = build_message(prices)
+            buttons = [
+                [{"text": "Creator on X",
+                    "url": "https://x.com/codeolam"}],
+                [{"text": "WikiCat on X",
+                    "url": "https://x.com/wikicatcoin"}],
+                [{"text": "🤖 Bot Playground",
+                    "url": "https://t.me/kodOlam_bot"}],
+            ]
+            await send_to_channel(msg, buttons)
+        except Exception:
+            traceback.print_exc()
         await asyncio.sleep(120)
 
 
